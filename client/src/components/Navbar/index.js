3538299e35a83cea64c6870d570b91926ae948ab
Taylor <UkrainianEagle@q.com>
2019-03-29 16:24:23 -0500

    Added Carousel & Title components, edited Navbar comp. and App.js

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -3,7 +3,6 @@ import "./style.css";
 
 function Navbar() {
 	return (
-
 		<nav className="navbar navbar-expand-lg navbar-light bg-light">
 			<div className="navbar-brand">[Logo]</div>
 
@@ -12,9 +11,7 @@ function Navbar() {
 					<li className="nav-item active">
 						<a className="nav-link">Rendezvous <span className="sr-only">(current)</span></a>
 					</li>
-					{/* <li className="nav-item">
-						<a className="nav-link" href="#">Link</a>
-					</li> */}
+					
 					<li className="nav-item dropdown">
 						<a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 							Dropdown
