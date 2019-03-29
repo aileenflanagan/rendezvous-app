830450699c55fd9f5766847d6c8e2ed2056f4e46
DavidFarnick <dfarnick2@gmail.com>
2019-03-29 13:46:50 -0500

    first

---
+++ models/comment.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+    userId: { type: String, required: true },
+    description: { type: String, required: true},
+    date: { type: Date, default: Date.now },
+    like: { type: String },
+    dislike: { type: String }
+});
+
+const Comment = mongoose.model("Comment", commentSchema);
+
+module.exports = Comment;
