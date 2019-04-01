7a7528010f6b4d7c6e9b4df133527040a4daeab6
rtonn <rtonn09umn@gmail.com>
2019-04-01 13:02:31 -0500

    updates

--- routes/api/groups.js
+++ routes/api/groups.js
@@ -1,8 +1,12 @@
 const router = require("express").Router();
+var express = express.Router()
+var groups = require('../models/groups')
 //controller for 
 
 // this is the get all /api/groups route
-router.route('/')
+router.get('/', function(req, res){
+    groups.find()
+})
     .get()
     .post()
 
@@ -10,3 +14,7 @@ router.route('/')
 router.route('/:id')
     .get()
     .post()    
+
+
+
+module.exports = router
