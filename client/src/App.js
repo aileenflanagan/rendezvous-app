7fcdc2a4c240ad1bb918bab8618d782c4c5f42aa
Aileen Flanagan <aileenmflanagan@hotmail.com>
2019-04-02 14:10:00 -0500

    added exact paths in app.js and created pages folders

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,40 @@ import './App.css';
 import Home from "./pages/Home/index";
 import Group from "./pages/Group/index";
 import Signup from "./pages/Signup/index";
+import CreateGroup from "./pages/CreateGroup/index";
+import Search from "./pages/Search/index";
+import Manage from "./pages/ManageGroups/index"
 
-class App extends Component {
-  state = {
-    // get info from database and set state
-  }
+const App = () => (
+  <Router>
+    <div>
+      <NavBar/>
+      <Route exact path ="/" component= {Home}/>
+      <Route exact path="/group" component={Group} />
+      <Route exact path="/signup" component={Signup} />
+      <Route exact path="/createGroup" component={CreateGroup}/>
+      <Route exact path="/search" component={Search}/>
+      <Route exact path="/manage" component={Manage}/>
+    </div>
+  </Router>
+);
 
-  render() {
-    return (<>
-      {/* TODO: create routes for each page */}
-      {/* <Home /> */}
-      {/* <Group /> */}
-      <Signup />
-    </>
-    );
-  }
-}
+
+
+// class App extends Component {
+//   state = {
+//     // get info from database and set state
+//   }
+
+//   render() {
+//     return (<>
+//       {/* TODO: create routes for each page */}
+//       {/* <Home /> */}
+//       {/* <Group /> */}
+//       <Signup />
+//     </>
+//     );
+//   }
+// }
 
 export default App;
