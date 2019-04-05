d9f264daf7ba08105de8d317da650803d690b1aa
Gnarnold <nickrnld56@gmail.com>
2019-04-05 13:07:26 -0500

    User creation

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,27 +2,43 @@ const db = require('../models');
 
 module.exports = {
     create: function(req, res){
-        db.User
-            .create(req.body)
-            // .then(dbModel => res.json(dbModel))
-            .catch(err => console.log(err));
+        var username = req.body.username;
+        var email = req.body.email;
+        var password = req.body.password;
+        var location = req.body.location;
+
+        var newUser = new db.User({
+            userName: username,
+            email: email,
+            password: password,
+            location: location          
+        })  
+
+        db.User.hashPass(newUser, function(err, user){
+            if(err) throw err;
+            // console.log(user)
+            db.User
+                .create(user)
+                .then(data => res.josn(data))
+                .catch(err => console.log(err));
+        })        
     },
     update: function(req, res){
         db.User
             .find(req.query)
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.status(422).json(err));
+            .then(data => res.json(data))
+            .catch(err => console.log(err));
     },
     remove: function(req, res){
         db.User
             .find(req.query)
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.status(422).json(err));
+            .then(data => res.json(data))
+            .catch(err => console.log(err));
     },
     find: function(req, res){
         db.User
             .find(req.query)
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.status(422).json(err));
+            .then(data => res.json(data))
+            .catch(err => console.log(err));
     }
 }
