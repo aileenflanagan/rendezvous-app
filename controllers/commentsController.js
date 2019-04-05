10c5e7f393647e4add836d46b4ca6f1ac1c529f8
Gnarnold <nickrnld56@gmail.com>
2019-04-05 12:48:07 -0500

    user create fix

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -2,26 +2,26 @@ const db = require("../models");
 
 module.exports = {
     findAll: function(req, res) {
-        db.Comments
+        db.Comment
             .find(req.query)
             .sort({ date: -1 })
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     },
     findById: function(req, res) {
-        db.Comments
+        db.Comment
         .findById(req.params.id)
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));
     },
     create: function(req, res) {
-        db.Comments
+        db.Comment
             .create(req.body)
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     },
     remove: function(req, res){
-        db.Comments
+        db.Comment
         .findById({ _id: req.params.id}, req.body)
         .then(dbModel => dbModel.remove())
         .then(dbModel => res.json(dbModel))
--- routes/api/users2.js
+++ routes/api/users2.js
@@ -1,7 +1,16 @@
 const router = require("express").Router();
 const User = require('../../models/user');
 const userController = require("../../controllers/userController");
+
+const cloudinary = require('cloudinary')
+cloudinary.config({ 
+  cloud_name: "dqadqluxx", 
+  api_key: "618381916967669", 
+  api_secret: "1bomeODECnYIR817uCouYk1SqPM"
+})
+
 const passport = require('passport');
+
 
 //login route /api/users/login
 router.route("/login")
@@ -20,6 +29,17 @@ router.route('/logout')
 router
   .route("/register")
   .post(userController.create);
+
+router.route("/userSave").post(function(req, res) {
+  console.log("we hit the route", req.body)
+  const values = Object.values(req.files)
+  const promises = values.map(image => cloudinary.uploader.upload(image.path))
+  
+  Promise
+    .all(promises)
+    .then(results => console.log('these are our results!!!', results))
+  // console.log("heres our pics", values)
+})  
 
 module.exports = router;
 
