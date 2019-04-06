249c07c45acb0de4a379241afebf47d7b4910312
rtonn <rtonn09umn@gmail.com>
2019-04-06 16:08:41 -0500

    group updates

--- controllers/groupController.js
+++ controllers/groupController.js
@@ -1,43 +1,48 @@
 const db = require('../models');
 
 module.exports = {
+    create: function(req, res){
+    db.Groups
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err))
+    },
+
     findAll: function(req, res){
-        db.Group
+        db.Groups
             .find(req.query)
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     },
-
-    findByKeywords: function(req, res){
-        console.log("findbykeywords, groupcontroller");
-        db.Group
-            .find({groupName: { $regex: `${req.params.keyword}`, $options: "i" }})
-            .then(dbModel => res.json(dbModel), console.log("On: groupController.js"))
-            .catch(err => res.status(422).json(err));
-    },
-
-    // findByZip: function(req, res){
-    //     db.Groups
-    //         .find()
-    //         .then()
-    //         .catch(err => res.status(422).json(err));
-    // },
-    create: function(req, res){
-        db.Group
-            .create(req.body)
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.status(422).json(err));
-    },
+ 
+//....................................................
+    
     update: function(req, res) {
-        db.Group
+        db.Groups
         .findOneAndUpdate({_id: req.params.id}, req.body)
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     },
-    remove: function(req, res){
-        db.Group
-        .find(req.query)
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.status(422).json(err));
-    }
+    remove: function(req, res) {
+        db.Groups.findById(req.params.id)
+          .then(dbGroups => dbGroups.remove())
+          .then(dbGroups => res.json(dbGroups))
+          .catch(err => res.status(422).json(err));
+      },
+
+//........................................................
+
+findById: function(req, res) {
+    db.Groups.findById(req.params.id)    
+      .then(dbGroups => res.json(dbGroups))
+      .catch(err => res.status(422).json(err));
+  },
+
+//........................................................
+
+    findByKeywords: function(req, res){
+        db.Groups
+            .find({groupName: { $regex: `${req.params.searchTerm}`, $options: "i" }})
+    },
+    
 }
