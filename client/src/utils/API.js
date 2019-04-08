470b06838e88367c28e29ee9091377a6cd00e2bc
Taylor <UkrainianEagle@q.com>
2019-04-07 21:23:24 -0500

    Updated API.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,15 +3,6 @@ import axios from "axios";
 export default {
 
 	findById: function(id) {
-<<<<<<< HEAD
-=======
-		console.log("On: API.js");
-		console.log("id:", id);
-		return axios.get("/api/groups/" + id);
-	},
-	
-	findByKeywords: function (keyword) {
->>>>>>> 721cf053e2f8c77383687d14d1762368144d67e4
 		console.log("On: API.js");
 		console.log("id:", id);
 		return axios.get("/api/groups/" + id);
