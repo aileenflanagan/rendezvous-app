1b7acd28b00ead88b3119e90bfd1c9682202eee1
Aileen Flanagan <aileenmflanagan@hotmail.com>
2019-04-08 15:08:10 -0500

    some updates to the manage groups

--- client/src/components/GroupCard/index.js
+++ client/src/components/GroupCard/index.js
@@ -2,25 +2,26 @@ import React from "react";
 import "./style.css";
 
 function GroupCard(props) {
-
+	console.log("props ",props);
 	return (
-		<div className="group-card-container">
-			{props.groups.map(group => (
-				<div key={group._id} className="group-cards">
-					<img id="img-container" alt="" src="https://www.akc.org/wp-content/themes/akc/component-library/assets/img/register.jpg"/>
-					<h4 className="group-names">{group.groupName}</h4>
+		// <div className="group-card-container">
+		// 	{props.groups.map(group => (
+		// 		<div key={group._id} className="group-cards">
+		// 			<img id="img-container" alt="" src="https://www.akc.org/wp-content/themes/akc/component-library/assets/img/register.jpg"/>
+		// 			<h4 className="group-names">{group.groupName}</h4>
 
-					{/* <a href="/group"> */}
-					<button
-						id={group._id}
-						onClick={() => props.findById(group._id)}
-					>View this group
-					</button>
-					{/* </a> */}
-				</div>
-			))}
+		// 			{/* <a href="/group"> */}
+		// 			<button
+		// 				id={group._id}
+		// 				onClick={() => props.findById(group._id)}
+		// 			>View this group
+		// 			</button>
+		// 			{/* </a> */}
+		// 		</div>
+		// 	))}
 
-		</div>
+		// </div>
+		<p>hi</p>
 	);
 }
 
--- client/src/components/Manage/index.js
+++ client/src/components/Manage/index.js
@@ -1,28 +1,67 @@
-import React from "react";
+import React, { Component } from "react";
 import "./style.css";
 import GroupInfo from "../GroupInfo/index";
 import NavbarBasic from "../NavbarBasic";
 import AccountInfo from "../AccountInfo";
 import GroupCard from "../GroupCard";
 import axios from "axios";
+import API from "../../utils/API";
 
-
+let editing=false;
+let changingBtnTxt="Edit";
 // import user data from db
 // import group data from db
 
 
 function clickHandler() {
-	console.log("Event for editing goes here");
+	console.log("Event for editing goes here", editing);
+		editing=!editing;
+		console.log(editing);
 }
 
 
-class ManageGroups extends React.Component {
+class ManageGroups extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			image:""
+			image:"",
+			name:"",
+			email:"",
+			zip:"",
+			password:"",
+			groupId:[], 
+			groups:[],
+			admin:[]
 		}
 	}
+
+	componentDidMount=()=>{
+		this.loadInfo();
+	}
+
+	loadInfo=(id)=>{
+		API.findByUserId("5cab86375fb512895b1f91f8")
+		.then(res => {
+			console.log("res.data " ,res.data);
+			this.setState({
+				name:res.data.userName,
+				image: res.data.image,
+				email: res.data.email,
+				zip:res.data.zip,
+				password: res.data.password,
+				groupId: res.data.groupId,
+				admin: res.data.admin
+			})
+			console.log(this.state);
+		})
+		.then(
+			
+		)
+		.catch(err => console.log(err));
+	}
+
+
+
 	fileSelectedHandler = (event) => {
 		console.log("this is our photo", event.target.files[0])
 		const files = Array.from(event.target.files)
@@ -46,6 +85,8 @@ class ManageGroups extends React.Component {
 	
 	return (<>
 		<NavbarBasic />
+		
+
 		<div className="row">
 			<div className="col-md-1"></div>
 			{/* Profile Pic */}
@@ -60,14 +101,15 @@ class ManageGroups extends React.Component {
 				<div className="row" id="edit-row">
 					<div className="col-md-10" id="settings-div">
 						<AccountInfo
-							name={"this.state.name"}
-							email={"this.state.email"}
-							location={"this.state.location"}
-							password={"this.state.password"}
+							name={this.state.name}
+							email={this.state.email}
+							zip={this.state.zip}
+							password="******"
 						/>
 					</div>
 					<div className="col-md-2" id="edit-btn-div">
-						<button onClick={clickHandler} className="btn btn-primary" id="edit-btn">Edit</button>
+						{editing? <button onClick={clickHandler}>Save</button>: <button onClick={clickHandler}>edit</button>}
+						{/* <button onClick={clickHandler} className="btn btn-primary" id="edit-btn">{editing? "Save": "Edit"}</button> */}
 					</div>
 				</div>
 			</div>
@@ -79,8 +121,12 @@ class ManageGroups extends React.Component {
 			<div className="col-md-10" id="joined-groups-div">
 
 				<div>[group card component goes here: group image, group name]</div>	{/* temporary */}
-
-				<GroupCard />
+				<p>{this.state.groups}groups</p>
+				<GroupCard 
+						groups={this.state.groups}
+						
+					
+					/>
 				{/* <GroupCard />
 					<GroupCard />
 					<GroupCard />
@@ -101,9 +147,9 @@ class ManageGroups extends React.Component {
 			<div className="col-md-1"></div>
 			<div className="col-md-10" id="created-groups-div">
 				<div>[group card component goes here: group image, group name, reassign btn, delete btn]</div>	{/* temporary */}
-				<GroupCard
+				{/* <GroupCard
 					groupName={"props.groupName"}
-				/>
+				/> */}
 
 
 			</div>
