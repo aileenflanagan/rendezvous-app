16ea1969f1bd1cf1ad59f88d6bab2bd9a82775be
Aileen Flanagan <aileenmflanagan@hotmail.com>
2019-04-09 16:47:47 -0500

    another tweak, hopefully will deploy

--- server.js
+++ server.js
@@ -44,6 +44,10 @@ app.use(passport.session());
 // Add routes, both API and view
 app.use(routes);
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
 // Start the API server
 app.listen(PORT, function() {
   console.log(` API Server now listening on PORT ${PORT}!`);
